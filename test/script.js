584f045ce0dc97722b863adfe7d23467bf2dbba3
yusuke-kitayama <yusktym@ab.auone-net.jp>
2018-04-13 11:35:55 +0900

    test up

---
+++ test/script.js
@@ -0,0 +1,9 @@
+function oshirase(num){
+return "あなたの会員Noは_" + num +"_です。<br> 忘れないようにメモをとるなどして大切に保管してください。" ;
+}
+
+document.write(oshirase(1002)+"<br>");
+document.write(oshirase(1028)+"<br>");
+document.write(oshirase(2017)+"<br>");
+
+
